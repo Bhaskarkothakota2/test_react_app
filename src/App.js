9e017294deafe6fcafbdde3ce3fd45e9e7f95bb7
bhaskarkothakota2 <bhaskar.eidiko@gmail.com>
2022-05-18 15:41:27 +0530

    added envs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>This {process.env.REACT_APP_ID}</h1>
+      <h1>This {process.env.REACT_APP_NAME}</h1>
     </div>
   );
 }
